cd86e8d313f0d44ceb9baf177b9e968d1b1a1ade
Umang <umy22@users.noreply.github.com>
2022-05-05 23:31:45 +0530

    Add files via upload
    Base Implementation of Gilded Rose Kata - Refactoring Exercise :
    
    - The application is made of 2 classes which are GildedRoseKata class & the Item class
    
    - Third class we can consider as Test class (Trigger_Test)
    
    - Will follow below two constraint-
       1) Not alter the Item class
       2) Not alter the Item property
    - While Observing implementation code it contains nested if-statement, long & complicated hard-coded strings.

---
+++ GildedRoseKata_Refactoring/src/com/test/java/Trigger_Test.java
@@ -0,0 +1,39 @@
+package com.test.java;
+
+import com.main.java.GildedRoseKata;
+import com.main.java.Item;
+
+public class Trigger_Test {
+
+	public static void main(String[] args) {
+		
+        Item[] items = new Item[] {
+              
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                new Item("Conjured Mana Cake", 2, 5) 
+        };
+
+        GildedRoseKata app = new GildedRoseKata(items);
+
+        int days = 2;
+        if (args.length > 0) {
+            days = Integer.parseInt(args[0]) + 1;
+        }
+
+        for (int i = 0; i < days; i++) {
+            System.out.println("-------- day " + i + " --------");
+            System.out.println("name, sellIn, quality");
+            for (Item item : items) {
+                System.out.println(item);
+            }
+            System.out.println();
+            app.updateQuality();
+        }
+	}
+}
