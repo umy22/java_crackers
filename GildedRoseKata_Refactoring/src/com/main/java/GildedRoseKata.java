1046de40583cc8af724eaefbede53c8f4a102182
Umang <umy22@users.noreply.github.com>
2022-05-06 20:12:28 +0530

    Update GildedRoseKata.java
    3) In updateQuality() method now we are divided 'if condition' into two different method.
    -  updateQuality(item) - Polymorphism implemented here
    -  With this step we are dividing our complex logic into method Separation. 
    
    Old :-  if (!item.name.equals("Aged Brie")
                     && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (item.quality > 0) {
                     if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                     	item.quality = item.quality - 1;
                     }
                 }
             } else {
                 if (item.quality < 50) {
                 	item.quality = item.quality + 1;
    
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
                             	item.quality = item.quality + 1;
                             }
                         }
    
                         if (item.sellIn < 6) {
                             if (item.quality < 50) {
                             	item.quality = item.quality + 1;
                             }
                         }
                     }
                 }
             }
    
    New :-   private void updateQuality(Item item) {
    		// Above if condition 
    	}

--- GildedRoseKata_Refactoring/src/com/main/java/GildedRoseKata.java
+++ GildedRoseKata_Refactoring/src/com/main/java/GildedRoseKata.java
@@ -11,41 +11,17 @@ public class GildedRoseKata {
     
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    	item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                	item.quality = item.quality + 1;
-
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                            	item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                            	item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            	item.sellIn = item.sellIn - 1;
-            }
-
-           checkIfExpired(item);
+           
+	           updateQuality(item);
+	           
+	           if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+	            	item.sellIn = item.sellIn - 1;
+	            }
+	           checkIfExpired(item);
         }
     }
     
+   
     private void checkIfExpired(Item item) {
    	 if (item.sellIn < 0) {
             if (!item.name.equals("Aged Brie")) {
@@ -64,5 +40,35 @@ public class GildedRoseKata {
                 }
             }
         }
-   }
+    }
+    
+    
+    private void updateQuality(Item item) {
+    	 if (!item.name.equals("Aged Brie")
+                 && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+             if (item.quality > 0) {
+                 if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                 	item.quality = item.quality - 1;
+                 }
+             }
+         } else {
+             if (item.quality < 50) {
+             	item.quality = item.quality + 1;
+
+                 if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                     if (item.sellIn < 11) {
+                         if (item.quality < 50) {
+                         	item.quality = item.quality + 1;
+                         }
+                     }
+
+                     if (item.sellIn < 6) {
+                         if (item.quality < 50) {
+                         	item.quality = item.quality + 1;
+                         }
+                     }
+                 }
+             }
+         }
+    }
 }
