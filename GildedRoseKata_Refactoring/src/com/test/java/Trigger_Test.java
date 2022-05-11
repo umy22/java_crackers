ba243dc9576ebd0cd151c65a03462bb1b779366d
Umang <umy22@users.noreply.github.com>
2022-05-11 11:01:13 +0530

    Update Trigger_Test.java
    Converted Item[] array to ArrayList<Item>

--- GildedRoseKata_Refactoring/src/com/test/java/Trigger_Test.java
+++ GildedRoseKata_Refactoring/src/com/test/java/Trigger_Test.java
@@ -1,5 +1,7 @@
 package com.test.java;
 
+import java.util.ArrayList;
+
 import com.main.java.GildedRoseKata;
 import com.main.java.Item;
 
@@ -7,18 +9,18 @@ public class Trigger_Test {
 
 	public static void main(String[] args) {
 		
-        Item[] items = new Item[] {
-              
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                new Item("Conjured Mana Cake", 2, 5) 
-        };
+		
+		ArrayList<Item> items = new ArrayList<Item>();
 
+		items.add(new Item("Aged Brie", 2, 0));
+		items.add(new Item("Elixir of the Mongoose", 5, 7));
+		items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+		items.add(new Item("Sulfuras, Hand of Ragnaros", -1, 80));
+		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49));
+		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49));
+		items.add(new Item("Conjured Mana Cake", 2, 5));
+        
         GildedRoseKata app = new GildedRoseKata(items);
 
         int days = 2;
